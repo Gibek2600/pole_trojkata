f7062d784e3f8207b5e81b986b278ac6f77e0b82
Michał Gibczyński <Gibek2600@interia.pl>
2018-02-03 18:18:30 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+var a = prompt ("a value");
+var h = prompt ("h value");
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
